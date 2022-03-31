87c2a4d17adcee4926f7c6718470ec931e9236a8
tkcambs <tom_kelk@tomk-instance.europe-north1-b.c.lbg-210322.internal>
2022-03-31 15:10:37 +0000

    updated price error

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ let id = 1;
 
 // Function to build an item
 // takes in a name, description and price
-let itemBuilder = (itemName=TaylorMadeStealth, itemDescription=Driver, itemPrince=499, itemID=1234) => {
+let itemBuilder = (itemName=TaylorMadeStealth, itemDescription=Driver, itemPrice=499, itemID=1234) => {
     // uses literal notation to create an item object
     let item = {
         name : itemName,
